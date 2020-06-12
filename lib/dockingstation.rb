004f6a1eabca16ebbd16ec5c1c6432de454b800d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-12 18:40:11 +0100

    Refactors to adhrere to the single responsibility principle - makes two private methodsg

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -10,14 +10,24 @@ class DockingStation
   end 
 
   def dock(bike)
-    fail "Unable to dock bike. Station is at full capacity" if @bikes.length >= 5
+    fail "Unable to dock bike. Station is at full capacity" if full?
     @bikes.push(bike)
   end 
 
 
   def release_bike
-    fail "No bikes available" unless @bikes.length >= 1
+    fail "No bikes available" if empty?
     @bikes.pop
+  end 
+
+  private
+
+  def full?
+    return true if @bikes.length >= 5
+  end
+
+  def empty?
+    return true if @bikes.length == 0
   end 
 
 
