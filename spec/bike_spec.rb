b788323c36e0df7aec78ed12b10b3921561dd142
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-12 15:55:39 +0100

    Bikes that are released work

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,13 @@ require 'bike'
 describe Bike do  
 
   describe 'working?' do
+
     it { is_expected.to respond_to :working? }
+
+    it "works" do
+      bike = Bike.new
+      expect(bike.working?).to eq(true)
+    end
 
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,24 @@
 require 'dockingstation'
 
 describe DockingStation do 
+    let(:bike) { double(:bike) }
+  
     
   describe '#release_bike' do
 
-    let(:bike) { double(:bike) }
-    
     it { is_expected.to respond_to :release_bike }
 
     it 'can release a bike' do
       dockingstation = DockingStation.new
       expect(dockingstation.release_bike(bike)).to eq bike 
     end 
+
+    it 'can release a bike that works' do
+      dockingstation = DockingStation.new
+      bike = double('bike', {working?: 'true'})
+      dockingstation.release_bike(bike)
+      expect(bike.working?).to eq('true')
+    end
   end 
 
 end 
